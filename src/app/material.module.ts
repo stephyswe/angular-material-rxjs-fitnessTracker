9d85c3e70b91df2c27e16521b0cbcff4f097b1bf
Stephanie <stephaniiehallberg@gmail.com>
2019-08-12 17:10:40 +0200

    Splitting the Navigation Into Components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,16 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
